6fa3938bd4e2ad1e3223e977566da39d224e271c
Dominic W <47263379+Domsmasher1@users.noreply.github.com>
2024-06-10 23:26:09 +1000

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 set -o errexit -o nounset
+
+# Navigate to the app directory
 cd DiceCloud/app
+
+# Install npm dependencies
 meteor npm install
-meteor
+
+# Start the Meteor application
+exec meteor
